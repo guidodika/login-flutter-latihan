77dbe782c3cabf3ddc8d7ec80011058fb3f5d483
Arman La Ama <arman.ama@k24.co.id>
2023-07-06 14:46:55 +0700

    first commit

---
+++ lib/LoginPage.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+
+void main() {
+  runApp(LoginPage());
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool isLoginFormVisible = true;
+  double containerPosition = 0;
+
+  void toggleLoginFormVisibility() {
+    setState(() {
+      isLoginFormVisible = !isLoginFormVisible;
+      containerPosition = isLoginFormVisible ? 0 : -300;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Latihan Mobile App K-24',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: Stack(
+          children: [
+            // Background Image
+            Positioned.fill(
+              child: Image.asset(
+                'assets/background-image-k24.jpg',
+                fit: BoxFit.cover,
+              ),
+            ),
+            // Text Overlay
+            Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(20, 60, 20, 0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'K-24',
+                      style: TextStyle(
+                        fontSize: 60,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.red,
+                      ),
+                    ),
+                    Text(
+                      'Pasti Peduli',
+                      style: TextStyle(
+                        fontSize: 30,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            // Login Form
+            AnimatedPositioned(
+              duration: Duration(milliseconds: 500),
+              curve: Curves.easeInOut,
+              left: 0,
+              right: 0,
+              bottom: containerPosition,
+              child: GestureDetector(
+                onVerticalDragUpdate: (details) {
+                  // Slide the login form up or down based on drag movement
+                  if (details.delta.dy < 0 && !isLoginFormVisible) {
+                    toggleLoginFormVisibility();
+                  } else if (details.delta.dy > 0 && isLoginFormVisible) {
+                    toggleLoginFormVisibility();
+                  }
+                },
+                child: Container(
+                  padding: EdgeInsets.all(30),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
+                    ),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Text(
+                        'Selamat Datang',
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.red,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        'Silahkan masuk dengan akun yang sudah diberikan',
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Username',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'PIN',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                        ),
+                        obscureText: true,
+                      ),
+                      Text(
+                        'PIN awal anda adalah 1111',
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.blue,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      RaisedButton(
+                        onPressed: () {
+
+                          // Tampilkan pesan toast
+                          Fluttertoast.showToast(
+                              msg: "Telah Berhasil yeayyyy",
+                              toastLength: Toast.LENGTH_SHORT,
+                              gravity: ToastGravity.CENTER,
+                              backgroundColor: Colors.green,
+                              textColor: Colors.white,
+                              fontSize: 16.0
+                          );
+                        },
+                        child: Text('Login'),
+                        color: Colors.blue,
+                        textColor: Colors.white,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

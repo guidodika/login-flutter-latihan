f697b79eb5c685f00b6e820999f58b153829b0b3
“Guido <guido.dika@k24.co.id>
2023-07-11 15:26:49 +0700

    removed unused widget

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -56,161 +56,158 @@ class _LoginPageState extends State<LoginPage> {
                 WillPopScopeHelper.dialogCLoseApp(context: context));
       },
       child: Scaffold(
-        body: BlocProvider(
-          create: (context) => _loginBloc,
-          child: Form(
-            key: _formKey,
-            child: Stack(
-              children: [
-                // Background Image
-                Positioned.fill(
-                  child: Image.asset(
-                    'assets/images/background-image-k24.jpg',
-                    fit: BoxFit.cover,
+        body: Form(
+          key: _formKey,
+          child: Stack(
+            children: [
+              // Background Image
+              Positioned.fill(
+                child: Image.asset(
+                  'assets/images/background-image-k24.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              // Text Overlay
+              Align(
+                alignment: Alignment.topLeft,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 60, 20, 0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        'K-24',
+                        style: TextStyle(
+                          fontSize: 60,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.red,
+                        ),
+                      ),
+                      Text(
+                        'Pasti Peduli',
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                // Text Overlay
-                Align(
-                  alignment: Alignment.topLeft,
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(20, 60, 20, 0),
+              ),
+              // Login Form
+              AnimatedPositioned(
+                duration: const Duration(milliseconds: 500),
+                curve: Curves.easeInOut,
+                left: 0,
+                right: 0,
+                bottom: containerPosition,
+                child: GestureDetector(
+                  onVerticalDragUpdate: (details) {
+                    // Slide the login form up or down based on drag movement
+                    if (details.delta.dy < 0 && !isLoginFormVisible) {
+                      toggleLoginFormVisibility();
+                    } else if (details.delta.dy > 0 && isLoginFormVisible) {
+                      toggleLoginFormVisibility();
+                    }
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.all(30),
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        topRight: Radius.circular(30),
+                      ),
+                    ),
                     child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text(
-                          'K-24',
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        const Text(
+                          'Selamat Datang',
                           style: TextStyle(
-                            fontSize: 60,
-                            fontWeight: FontWeight.bold,
+                            fontSize: 24,
                             color: Colors.red,
+                            fontWeight: FontWeight.bold,
                           ),
                         ),
-                        Text(
-                          'Pasti Peduli',
+                        const Text(
+                          'Silahkan masuk dengan akun yang sudah diberikan',
                           style: TextStyle(
-                            fontSize: 30,
-                            color: Colors.white,
+                            fontSize: 12,
+                            color: Colors.blue,
+                            fontWeight: FontWeight.bold,
                           ),
                         ),
+                        const SizedBox(height: 20),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          child: TextFormFieldHelper.stylePrimary(
+                              label: 'Username',
+                              hintText: 'Username',
+                              emailValidator: true,
+                              textController: _usernameController,
+                              context: context),
+                        ),
+                        const SizedBox(height: 10),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          child: TextFormFieldHelper.stylePrimary(
+                              label: 'Password',
+                              hintText: 'kata sandi',
+                              obscureText: true,
+                              textController: _passController,
+                              context: context),
+                        ),
+                        const Text(
+                          'PIN awal anda adalah 1111',
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.blue,
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        CupertinoButton(
+                          padding: const EdgeInsets.all(10),
+                          minSize: 10,
+                          borderRadius: BorderRadius.circular(5),
+                          color: Colors.blue,
+                          child: const Text(
+                            "Login",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          onPressed: () {
+                            debugPrint("cek login");
+                            if (_formKey.currentState!.validate()) {
+                              _loginBloc.add(PostLogin(
+                                username: _usernameController.text,
+                                password: _passController.text,
+                              ));
+                            }
+                          },
+                        ),
+                        Builder(
+                          builder: (context) {
+                            return BlocListener<LoginBloc, LoginState>(
+                              listener: (context, state) {
+                                debugPrint('State changed: ${state.status}');
+                                if (state.status == 'success') {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(builder: (context) => DashboardPage()),
+                                  );
+                                }
+                              },
+                              child: Container(),
+                            );
+                          },
+                        ),
+
                       ],
                     ),
                   ),
                 ),
-                // Login Form
-                AnimatedPositioned(
-                  duration: const Duration(milliseconds: 500),
-                  curve: Curves.easeInOut,
-                  left: 0,
-                  right: 0,
-                  bottom: containerPosition,
-                  child: GestureDetector(
-                    onVerticalDragUpdate: (details) {
-                      // Slide the login form up or down based on drag movement
-                      if (details.delta.dy < 0 && !isLoginFormVisible) {
-                        toggleLoginFormVisibility();
-                      } else if (details.delta.dy > 0 && isLoginFormVisible) {
-                        toggleLoginFormVisibility();
-                      }
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.all(30),
-                      decoration: const BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(30),
-                          topRight: Radius.circular(30),
-                        ),
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          const Text(
-                            'Selamat Datang',
-                            style: TextStyle(
-                              fontSize: 24,
-                              color: Colors.red,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          const Text(
-                            'Silahkan masuk dengan akun yang sudah diberikan',
-                            style: TextStyle(
-                              fontSize: 12,
-                              color: Colors.blue,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          const SizedBox(height: 20),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 10),
-                            child: TextFormFieldHelper.stylePrimary(
-                                label: 'Username',
-                                hintText: 'Username',
-                                emailValidator: true,
-                                textController: _usernameController,
-                                context: context),
-                          ),
-                          const SizedBox(height: 10),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 10),
-                            child: TextFormFieldHelper.stylePrimary(
-                                label: 'Password',
-                                hintText: 'kata sandi',
-                                obscureText: true,
-                                textController: _passController,
-                                context: context),
-                          ),
-                          const Text(
-                            'PIN awal anda adalah 1111',
-                            style: TextStyle(
-                              fontSize: 12,
-                              color: Colors.blue,
-                            ),
-                          ),
-                          const SizedBox(height: 20),
-                          CupertinoButton(
-                            padding: const EdgeInsets.all(10),
-                            minSize: 10,
-                            borderRadius: BorderRadius.circular(5),
-                            color: Colors.blue,
-                            child: const Text(
-                              "Login",
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                            onPressed: () {
-                              debugPrint("cek login");
-                              if (_formKey.currentState!.validate()) {
-                                _loginBloc.add(PostLogin(
-                                  username: _usernameController.text,
-                                  password: _passController.text,
-                                ));
-                              }
-                            },
-                          ),
-                          Builder(
-                            builder: (context) {
-                              return BlocListener<LoginBloc, LoginState>(
-                                listener: (context, state) {
-                                  debugPrint('State changed: ${state.status}');
-                                  if (state.status == 'success') {
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(builder: (context) => DashboardPage()),
-                                    );
-                                  }
-                                },
-                                child: Container(),
-                              );
-                            },
-                          ),
-
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),

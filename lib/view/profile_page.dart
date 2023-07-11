58bff5d446d9a1569ce3bdaa38a00376652888fc
“Guido <guido.dika@k24.co.id>
2023-07-11 15:22:06 +0700

    add layout profile page

--- lib/view/profile_page.dart
+++ lib/view/profile_page.dart
@@ -1,25 +1,109 @@
 import 'package:flutter/material.dart';
 
+
 class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Ini adalah halaman Profil',
-              style: TextStyle(fontSize: 24),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Logika logout
-              },
-              child: Text('Logout'),
-            ),
-          ],
+    return MaterialApp(
+      title: 'Profile',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 150,
+                height: 150,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 1
+                  ),
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/image_profile.jpg'),
+                    fit: BoxFit.contain,
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              const Text(
+                'John Doe',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 10),
+              const Text(
+                'Online',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.green,
+                ),
+              ),
+              SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.person),
+                    onPressed: () {
+                      // Tambahkan logika untuk menu Update Profile di sini
+                    },
+                  ),
+                  SizedBox(width: 10),
+                  const Text(
+                    'Update Profile',
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.lock),
+                    onPressed: () {
+                      // Tambahkan logika untuk menu Update Password di sini
+                    },
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    'Update Password',
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.logout),
+                    onPressed: () {
+                      // Tambahkan logika untuk menu Logout di sini
+                    },
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    'Logout',
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );

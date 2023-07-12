832558745ab9dd6393744c1d3261e84ac1300ef4
“Guido <guido.dika@k24.co.id>
2023-07-12 14:58:24 +0700

    add behaviour logout profile and add text name and status user.

--- lib/view/profile_page.dart
+++ lib/view/profile_page.dart
@@ -1,109 +1,142 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:untitled/utils/constant.dart';
+import 'package:untitled/view/splash_screen.dart';
+import 'package:untitled/view_model/login/login_bloc.dart';
 
+const String imageProfile = "assets/images/test_image.svg";
 
 class ProfilePage extends StatelessWidget {
+  static LoginBloc _loginBloc = LoginBloc();
   @override
   Widget build(BuildContext context) {
+    _loginBloc = BlocProvider.of<LoginBloc>(context);
     return MaterialApp(
       title: 'Profile',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: 150,
-                height: 150,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 1
-                  ),
-                  image: DecorationImage(
-                    image: AssetImage('assets/images/image_profile.jpg'),
-                    fit: BoxFit.contain,
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
-              const Text(
-                'John Doe',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 10),
-              const Text(
-                'Online',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.green,
-                ),
-              ),
-              SizedBox(height: 30),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
+        body: BlocBuilder<LoginBloc, LoginState>(
+          builder: (context, state) {
+            // Mendapatkan nilai 'status' dari state.dataUser
+            bool isOnline = UserConfig.dataUser.status == '1';
+
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  IconButton(
-                    icon: Icon(Icons.person),
-                    onPressed: () {
-                      // Tambahkan logika untuk menu Update Profile di sini
-                    },
-                  ),
-                  SizedBox(width: 10),
-                  const Text(
-                    'Update Profile',
-                    style: TextStyle(
-                      fontSize: 18,
+                  Container(
+                    width: 150,
+                    height: 150,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 1,
+                      ),
+                    ),
+                    child: SvgPicture.asset(
+                      imageProfile,
+                      fit: BoxFit.contain,
                     ),
                   ),
-                ],
-              ),
-              SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  IconButton(
-                    icon: Icon(Icons.lock),
-                    onPressed: () {
-                      // Tambahkan logika untuk menu Update Password di sini
-                    },
-                  ),
-                  SizedBox(width: 10),
+                  SizedBox(height: 20),
                   Text(
-                    'Update Password',
-                    style: TextStyle(
-                      fontSize: 18,
+                    // state.dataUser.map((e) {
+                    //   return e.displayName;
+                    // }).join(),
+                    UserConfig.dataUser.displayName,
+                    style: const TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                ],
-              ),
-              SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  IconButton(
-                    icon: Icon(Icons.logout),
-                    onPressed: () {
-                      // Tambahkan logika untuk menu Logout di sini
-                    },
-                  ),
-                  SizedBox(width: 10),
+                  SizedBox(height: 10),
                   Text(
-                    'Logout',
+                    isOnline
+                        ? 'Online'
+                        : 'Offline', // Menampilkan 'Online' atau 'Offline' berdasarkan nilai 'status'
                     style: TextStyle(
-                      fontSize: 18,
+                      fontSize: 16,
+                      color: isOnline
+                          ? Colors.green
+                          : Colors
+                              .red, // Mengubah warna teks menjadi hijau jika 'Online', merah jika 'Offline'
                     ),
+                  ),
+                  SizedBox(height: 30),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.person),
+                        onPressed: () {
+                          // Tambahkan logika untuk menu Update Profile di sini
+                        },
+                      ),
+                      SizedBox(width: 10),
+                      const Text(
+                        'Update Profile',
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.lock),
+                        onPressed: () {
+                          // Tambahkan logika untuk menu Update Password di sini
+                        },
+                      ),
+                      SizedBox(width: 10),
+                      Text(
+                        'Update Password',
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.logout),
+                        onPressed: () {
+                          Future.delayed(const Duration(seconds: 1), () async {
+                            _loginBloc.add(LogoutApp());
+                            Navigator.of(context, rootNavigator: true).pushAndRemoveUntil(
+                              MaterialPageRoute(
+                                builder: (BuildContext context) {
+                                  return const SplashScreen(checkStatus: true);
+                                },
+                              ),
+                                  (_) => false,
+                            );
+                          });
+                        },
+                      ),
+                      SizedBox(width: 10),
+                      Text(
+                        'Logout',
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ],
-          ),
+            );
+          },
         ),
       ),
     );

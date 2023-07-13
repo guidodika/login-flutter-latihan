13923b1137b8442d0c2d76a388b7308ddc5a9ac1
“Guido <guido.dika@k24.co.id>
2023-07-13 13:47:28 +0700

    add file card item

---
+++ lib/view/card_item.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CardItem extends StatefulWidget {
+  final String name;
+  final String keluhan;
+  final String birthdate;
+
+  const CardItem({
+    required this.name,
+    required this.keluhan,
+    required this.birthdate,
+  });
+  @override
+  _CardItemState createState() => _CardItemState();
+}
+
+class _CardItemState extends State<CardItem> {
+  bool isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(8.0),
+      shape: isChecked
+          ? RoundedRectangleBorder(
+        side: BorderSide(color: Colors.blue, width: 2.0),
+        borderRadius: BorderRadius.circular(8.0),
+      )
+          : null,
+      child: ListTile(
+        leading: Checkbox(
+          value: isChecked,
+          onChanged: (value) {
+            setState(() {
+              isChecked = value!;
+            });
+          },
+        ),
+        title: Row(
+          children: [
+            Expanded(
+              child: Text(widget.name),
+            ),
+            IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: () {
+                // Aksi ketika tombol delete ditekan
+              },
+            ),
+          ],
+        ),
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Keluhan: '),
+                Expanded(
+                  child: Text(
+                    widget.keluhan,
+                    textAlign: TextAlign.right,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 4.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Tanggal Lahir: '),
+                Expanded(
+                  child: Text(
+                    widget.birthdate,
+                    textAlign: TextAlign.right,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+        onTap: () {
+          // Aksi ketika card ditekan
+        },
+      ),
+    );
+  }
+}
